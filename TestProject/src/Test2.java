97f8be578a4aa48277fdb770a609cc9683d23c4b
doga <doga@172.16.4.11>
2019-06-18 15:47:08 +0300

    added class Test2.java

---
+++ TestProject/src/Test2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Test2 {
+
+	
+		public static void main(String[] args) {
+			Scanner sc=new Scanner(System.in);
+			System.out.println("Bir sayi giriniz");
+			int sayi=sc.nextInt();
+			int sayac=0;
+			for(int i=1;i<=sayi;i++) {
+				
+				if(sayi % i==0)
+				
+					sayac++;
+				
+	}
+				
+				if(sayac==2) {
+		   System.out.println(sayi+ " asaldýr");
+	   }   
+		   else {
+			   System.out.println(sayi+ " asal deðildir");
+	   }
+	}
+
+}
